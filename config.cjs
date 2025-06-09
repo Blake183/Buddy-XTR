c320d9c407df870937087e409a5abf4be14aeffd
Blake183 <geoffreyblake183@gmail.com>
2025-06-09 19:45:08 +0300

    Update config.cjs
    Signed-off-by: Blake183 <geoffreyblake183@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Buddy;;;oBRVxQTL#Meq-1FjjPP83ZlnkHvA9ry0Ul7TXX8C87mOAudfPK70",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true, 
